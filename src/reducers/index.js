8fd42a75e7bbd4cca87bebc3609809c37adf7a80
Onesun15 <sunnysharma15@hotmail.com>
2017-11-17 17:26:53 -0500

    add request to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,28 @@
-import { FETCH_CHEESES_SUCCESS, FETCH_CHEESES_ERROR } from '../actions';
+import { FETCH_CHEESES_REQUEST, FETCH_CHEESES_SUCCESS, FETCH_CHEESES_ERROR } from '../actions';
 
 const initialState = {
   cheeses: [],
+  loading: false,
   error: null
 };
 
 export function cheesesReducer(state = initialState, action) {
-  if (action.type === FETCH_CHEESES_SUCCESS) {
+  if (action.type === FETCH_CHEESES_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true,
+      error: null
+    });
+  } 
+   else if (action.type === FETCH_CHEESES_SUCCESS) {
     return Object.assign({}, state, {
       cheeses: action.cheeses,
+      loading: false,
       error: null
     });
   } else if (action.type === FETCH_CHEESES_ERROR) {
     return Object.assign({}, state, {
       error: action.error,
+      loading: false,
     });
   }
   return state;
