dea1febaefc20e441111dfe5c65604d0844866ad
Onesun15 <sunnysharma15@hotmail.com>
2017-11-17 17:12:47 -0500

    refactored

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,20 @@
 import { API_BASE_URL } from '../config';
 
+export const FETCH_CHEESES_SUCCESS = 'FETCH_CHEESES_SUCCESS';
+const fetchCheesesSuccess = cheeses => ({
+  type: FETCH_CHEESES_SUCCESS,
+  cheeses
+});
 
-export const FETCH_CHEESES_REQUEST = 'FETCH_CHEESES_REQUEST'
-const fetchCheesesRequest = () => ({
-    type: FETCH_CHEESES_REQUEST,
-})
-
-export const FETCH_CHEESES_SUCCESS = 'FETCH_CHEESES_SUCCESS'
-const fetchCheesesSuccess = (cheeses) => ({
-    type: FETCH_CHEESES_SUCCESS,
-    cheeses
-})
-
-export const FETCH_CHEESES_ERROR = 'FETCH_CHEESES_ERROR'
-const fetchCheesesError = (error) => ({
-    type: FETCH_CHEESES_ERROR,
-    error
-})
-
+export const FETCH_CHEESES_ERROR = 'FETCH_CHEESES_ERROR';
+const fetchCheesesError = error => ({
+  type: FETCH_CHEESES_ERROR,
+  error
+});
 
 export const fetchCheeses = () => dispatch => {
-    fetch(`${API_BASE_URL}/cheeses`)
-        .then(res => res.json()) 
-        .then(cheeses => dispatch(fetchCheesesSuccess(cheeses)))
-        .catch(error => dispatch(fetchCheesesError(error)))   
-}
+  fetch(`${API_BASE_URL}/cheeses`)
+    .then(res => res.json())
+    .then(cheeses => dispatch(fetchCheesesSuccess(cheeses)))
+    .catch(error => dispatch(fetchCheesesError(error)));
+};
